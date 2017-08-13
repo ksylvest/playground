3ef581352f4214db6f8a0be8fee3e9547a7ed3a4
Kevin Sylvestre <kevin@ksylvest.com>
2017-08-13 13:01:34 -0700

    fixing rspec helpers

---
+++ spec/rails_helper.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+require 'rspec/rails'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true
+
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
