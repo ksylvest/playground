97172d422f1970e82d7a62c349e3115f8acd8915
Kevin Sylvestre <kevin@ksylvest.com>
2017-10-14 13:56:53 -0700

    using JWT

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,7 +1,18 @@
 class Session < ApplicationRecord
+  ALGORITHM = 'HS256'.freeze
+
   belongs_to :user
 
   after_initialize do
     self.token = SecureRandom.hex
+  end
+
+  def self.encode(payload, algorithm: ALGORITHM)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base, algorithm)
+  end
+
+  def self.decode(payload, algorithm: ALGORITHM)
+    result, = JWT.decode(payload, Rails.application.secrets.secret_key_base, algorithm, algorithm: algorithm)
+    result
   end
 end
