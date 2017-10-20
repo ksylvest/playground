2f00eb0d9b68b6c93a0fe61ab03c80e4cf900c73
Kevin Sylvestre <kevin@ksylvest.com>
2017-10-19 19:29:49 -0700

    WIP

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,46 +1,5 @@
 class ApplicationPolicy
-  attr_reader :user, :record
-
-  def initialize(user, record)
-    @user = user
-    @record = record
-  end
-
-  def index?
-    false
-  end
-
-  def show?
-    scope.where(:id => record.id).exists?
-  end
-
-  def create?
-    false
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    false
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    false
-  end
-
-  def scope
-    Pundit.policy_scope!(user, record.class)
-  end
-
   class Scope
-    attr_reader :user, :scope
-
     def initialize(user, scope)
       @user = user
       @scope = scope
@@ -49,5 +8,10 @@ class ApplicationPolicy
     def resolve
       scope
     end
+  end
+
+  def initialize(user, record)
+    @user = user
+    @record = record
   end
 end
