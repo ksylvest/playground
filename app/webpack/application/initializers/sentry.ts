024255888ac9d1e94426ef69e209fea80fdc240b
Kevin Sylvestre <kevin@ksylvest.com>
2019-06-04 14:32:00 -0700

    Annotate sentry releases
    Requires:
    
        heroku labs:enable runtime-dyno-metadata

--- app/webpack/application/initializers/sentry.ts
+++ app/webpack/application/initializers/sentry.ts
@@ -1,11 +1,13 @@
 import * as Sentry from "@sentry/browser";
 
 declare const ENVIRONMENT: "production" | "test" | "development";
+declare const RELEASE: string;
 declare const SENTRY_DSN: string | null;
 
 if (SENTRY_DSN) {
   Sentry.init({
     dsn: SENTRY_DSN,
     environment: ENVIRONMENT,
+    release: RELEASE,
   });
 }
