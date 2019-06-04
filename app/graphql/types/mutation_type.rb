e9d09c4adcc5045e76b6128068813ec9778da933
Kevin Sylvestre <kevin@ksylvest.com>
2019-06-03 23:34:19 -0700

    Mutation for destroying a session

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,7 @@ module Types
     field :login, mutation: ::Mutations::LoginMutation
     field :logout, mutation: ::Mutations::LogoutMutation
     field :signup, mutation: ::Mutations::SignupMutation
+
+    field :destroy_session, mutation: ::Mutations::Session::DestroyMutation
   end
 end
