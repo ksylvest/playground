846c51a6eb33fce6f8425e96d3bf25e04d272995
Kevin Sylvestre <kevin@ksylvest.com>
2019-06-06 18:18:41 -0700

    Define initializer + hook for action cable library

---
+++ app/webpack/application/utils/use_action_cable_subscription.ts
@@ -0,0 +1,30 @@
+import {
+  useEffect,
+  useState,
+} from "react";
+
+import { CLIENT } from "@application/libraries/actioncable";
+
+export enum ActionCableSubscriptionStatus {
+  Connected = "CONNECTED",
+  Disconnected = "DISCONNECTED",
+}
+
+export const useActionCableSubscription = <T>(to: ActionCable.ChannelNameWithParams, received: (data: T) => void) => {
+  const [status, setStatus] = useState<ActionCableSubscriptionStatus | undefined>(undefined);
+
+  useEffect(() => {
+    const subscription = CLIENT.subscriptions.create(to, {
+      connected: () => setStatus(ActionCableSubscriptionStatus.Connected),
+      disconnected: () => setStatus(ActionCableSubscriptionStatus.Disconnected),
+      received,
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [to]);
+
+  return {
+    status,
+  };
+};
