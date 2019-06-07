a842d956e7baec261f4f3d954a50c2b89b6ba23f
Kevin Sylvestre <kevin@ksylvest.com>
2019-06-06 18:50:28 -0700

    Fix sentry tracking for test / development

--- app/webpack/application/initializers/sentry.ts
+++ app/webpack/application/initializers/sentry.ts
@@ -1,10 +1,10 @@
 import * as Sentry from "@sentry/browser";
 
-declare const ENVIRONMENT: "production" | "test" | "development";
+declare const ENVIRONMENT: string;
 declare const RELEASE: string;
 declare const SENTRY_DSN: string | null;
 
-if (SENTRY_DSN) {
+if (SENTRY_DSN && ENVIRONMENT !== "test" && ENVIRONMENT !== "development") {
   Sentry.init({
     dsn: SENTRY_DSN,
     environment: ENVIRONMENT,
