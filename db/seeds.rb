5ee11f342f70c2456a80aec8dfff22b7c3456d33
Kevin Sylvestre <kevin@ksylvest.com>
2019-06-06 18:47:49 -0700

    Fix seeds for users / sessions / notifications

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 User.transaction do
-  User.new(name: 'John', email: 'john@beatles.com', password: 'secret').save!
-  User.new(name: 'Paul', email: 'paul@beatles.com', password: 'secret').save!
-  User.new(name: 'George', email: 'george@beatles.com', password: 'secret').save!
-  User.new(name: 'Ringo', email: 'ringo@beatles.com', password: 'secret').save!
+  passworded = ->(user) { user.password = SecureRandom.alphanumeric }
+  User.find_or_initialize_by(name: 'John', email: 'john@beatles.com', &passworded).save!
+  User.find_or_initialize_by(name: 'Paul', email: 'paul@beatles.com', &passworded).save!
+  User.find_or_initialize_by(name: 'George', email: 'george@beatles.com', &passworded).save!
+  User.find_or_initialize_by(name: 'Ringo', email: 'ringo@beatles.com', &passworded).save!
 end
 
 Session.transaction do
   User.all.each do |user|
-    Session.new(user: user, ip: '0.0.0.0').save!
+    Session.find_or_initialize_by(user: user, ip: '0.0.0.0').save!
+  end
+end
+
+Notification.transaction do
+  User.all.each do |user|
+    Notification.find_or_initialize_by(user: user, message: "Welcome #{user.name}").save!
+    Notification.find_or_initialize_by(user: user, message: "Goodbye #{user.name}").save!
   end
 end
