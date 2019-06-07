4a50fc29f419b34f3c0dc1957ccaf6fc67f39bce
Kevin Sylvestre <kevin@ksylvest.com>
2019-06-06 18:16:30 -0700

    Integration authorization for action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :user
+
+    def connect
+      Current.cookies = cookies
+      self.user = Current.user
+      reject_unauthorized_connection unless user
+    end
   end
 end
