d751bc9158d3e68a197f7dfc9a70c7015f5ac6f4
Kevin Sylvestre <kevin@ksylvest.com>
2019-06-22 10:44:57 -0700

    Install active storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Configure active storage service.
+  config.active_storage.service = :aws
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
