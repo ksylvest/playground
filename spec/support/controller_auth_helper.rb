4084f5a5792cf40fc5adaa083a23e3f6cfe96cd8
Kevin Sylvestre <kevin@ksylvest.com>
2019-08-11 18:42:45 -0700

    Delete unused controller auth helpers

--- spec/support/controller_auth_helper.rb
+++ spec/support/controller_auth_helper.rb
@@ -1,12 +1,4 @@
 module ControllerAuthHelper
   delegate :authenticate, to: :subject
   delegate :deauthenticate, to: :subject
-
-  def should_be_authenticated
-    expect(subject.user).not_to be_nil
-  end
-
-  def should_be_deauthenticated
-    expect(subject.user).to be_nil
-  end
 end
