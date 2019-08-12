5766c7e4aeeb78f4b38eb9634ae8b68ace96b9a9
Kevin Sylvestre <kevin@ksylvest.com>
2019-08-11 18:16:06 -0700

    Implement spec for graphql controller

--- spec/controllers/graphql_controller_spec.rb
+++ spec/controllers/graphql_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe GraphqlController, type: :controller do
-
+RSpec.describe GraphqlController, type: :request do
+  describe 'POST #execute' do
+    it 'is successful' do
+      post graphql_url, params: { query: '{}' }
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
