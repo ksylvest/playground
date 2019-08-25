c68e5704a0f704adc164f18e373ff6b0b474e2e8
Kevin Sylvestre <kevin@ksylvest.com>
2019-08-25 11:59:09 -0700

    Test billing type

---
+++ spec/graphql/types/billing_type_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Types::BillingType do
+  describe '#resolve' do
+    subject(:execute) do
+      Current.auth!(user: user)
+      AppSchema.execute(gql)
+    end
+
+    let(:gql) do
+      <<~GQL
+        query Billing {
+          billing {
+            customer {
+              id
+              currency
+              sources {
+                id
+                number
+                brand
+                exp
+                default
+              }
+            }
+          }
+        }
+      GQL
+    end
+
+    let!(:user) { create(:user) }
+    let!(:customer) { create(:billing_customer, user: user) }
+    let!(:source) { create(:billing_source, customer: customer) }
+
+    it 'resolves' do
+      expect(execute['errors']).to be_nil
+      expect(execute['data']['billing']).to be_present
+      expect(execute['data']['billing']['customer']).to be_present
+      expect(execute['data']['billing']['customer']['sources']).to be_present
+    end
+  end
+end
