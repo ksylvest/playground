363dc24b2b1157458c5d6b9884f2d9b8c9587672
Kevin Sylvestre <kevin@ksylvest.com>
2019-08-26 23:16:41 -0700

    Standardize shoulda syntax usage

--- spec/models/feed/like_spec.rb
+++ spec/models/feed/like_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Feed::Like, type: :model do
   subject { build(:feed_like) }
 
-  it { should belong_to :user }
-  it { should belong_to :entry }
+  it { should belong_to(:user) }
+  it { should belong_to(:entry) }
 end
