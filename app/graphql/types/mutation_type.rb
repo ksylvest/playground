5cb6488bf0e8d6b5e9d8b7b1f4d9e7fc9b337e57
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-08 14:19:00 -0700

    GQL mutation for commenting

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -20,6 +20,7 @@ module Types
     field :destroy_billing_source, mutation: ::Mutations::Billing::Source::DestroyMutation
     field :default_billing_source, mutation: ::Mutations::Billing::Source::DefaultMutation
 
+    field :build_feed_comment, mutation: ::Mutations::Feed::Comment::BuildMutation
     field :like_feed_entry, mutation: ::Mutations::Feed::Entry::LikeMutation
     field :unlike_feed_entry, mutation: ::Mutations::Feed::Entry::UnlikeMutation
   end
---
+++ spec/graphql/mutations/feed/comment/build_mutation_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe Mutations::Feed::Comment::BuildMutation do
+  describe '#resolve' do
+    subject :execute do
+      Current.auth!(user: user)
+      AppSchema.execute(gql, variables: { input: input })
+    end
+
+    let(:gql) do
+      <<~GQL
+        mutation BuildFeedComment($input: Feed__CommentInput!) {
+          buildFeedComment(input: $input) {
+            status
+            errors {
+              messages
+            }
+            comment {
+              id
+              message
+              user {
+                id
+                name
+              }
+            }
+          }
+        }
+      GQL
+    end
+
+    let(:input) do
+      {
+        entryID: entry.id,
+        message: message,
+      }
+    end
+
+    let(:user) { create(:user) }
+    let(:entry) { create(:feed_entry, user: user) }
+
+    context 'with valid input' do
+      let(:message) { 'The quick brown fox jumped over the lazy dog.' }
+
+      it 'resolves' do
+        expect(execute['errors']).to be_nil
+        expect(execute['data']['buildFeedComment']['status']).to eql('OK')
+        expect(execute['data']['buildFeedComment']['comment']).to be_present
+        expect(execute['data']['buildFeedComment']['comment']['message']).to eql(message)
+        expect(execute['data']['buildFeedComment']['comment']['user']).to be_present
+        expect(execute['data']['buildFeedComment']['comment']['user']['id']).to eql(user.id)
+        expect(execute['data']['buildFeedComment']['comment']['user']['name']).to eql(user.name)
+      end
+
+      it 'builds a comment' do
+        expect { execute }.to change { Feed::Comment.where(entry: entry).count }
+      end
+    end
+
+    context 'with invalid input' do
+      let(:message) { '' }
+
+      it 'resolves' do
+        expect(execute['errors']).to be_nil
+        expect(execute['data']['buildFeedComment']['status']).to eql('UNPROCESSABLE')
+        expect(execute['data']['buildFeedComment']['errors']).to be_present
+        expect(execute['data']['buildFeedComment']['errors']['messages']).to be_present
+      end
+
+      it 'does not build a comment' do
+        expect { execute }.not_to change { Feed::Comment.count }
+      end
+    end
+  end
+end
