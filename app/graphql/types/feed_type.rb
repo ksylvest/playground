9c1d8d8ce4570f68e082902a815aa34bebf62be7
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-13 17:50:11 -0700

    Fix invalid optional in FeedType

--- app/graphql/types/feed_type.rb
+++ app/graphql/types/feed_type.rb
@@ -1,9 +1,9 @@
 module Types
   class FeedType < GraphQL::Schema::Object
-    field :entry, Types::Feed::EntryType, null: true do
+    field :entry, Types::Feed::EntryType, null: false do
       argument :id, ID, required: true
     end
-    field :entries, [Types::Feed::EntryType], null: true
+    field :entries, [Types::Feed::EntryType], null: false
 
     def entry(id:)
       object.entries.find(id)
