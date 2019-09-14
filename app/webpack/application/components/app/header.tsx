80eef25a9338ab52ae55aa78bc3cff6836471275
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-13 21:20:29 -0700

    Reduce complexity with app layout

--- app/webpack/application/components/app/header.tsx
+++ app/webpack/application/components/app/header.tsx
@@ -12,71 +12,90 @@ import { LOGIN_URL, NOTIFICATIONS_URL, ROOT_URL, SETTINGS_URL, SIGNUP_URL } from
 
 const DEFAULT_ACTIVE = false;
 
-export const Header: React.FC = () => {
-  const { session, stats } = useContext(World);
-  const [active, setActive] = useState<boolean>(DEFAULT_ACTIVE);
+const NavbarLink: React.FC<{ exact?: boolean; to: string }> = ({ exact, to, children }) => (
+  <NavLink exact={exact} className="navbar-item" to={to} activeClassName="is-active">
+    {children}
+  </NavLink>
+);
 
+const HomeNavbarLink: React.FC = () => (
+  <NavbarLink exact to={ROOT_URL}>
+    Playground
+  </NavbarLink>
+);
+
+const LoginNavbarLink: React.FC = () => (
+  <NavbarLink exact to={LOGIN_URL}>
+    Login
+  </NavbarLink>
+);
+
+const SignupNavbarLink: React.FC = () => (
+  <NavbarLink exact to={SIGNUP_URL}>
+    Signup
+  </NavbarLink>
+);
+
+const AuthenticateNavbarLinks: React.FC = () => (
+  <>
+    <LoginNavbarLink />
+    <SignupNavbarLink />
+  </>
+);
+
+const NotificationsNavbarLink: React.FC = () => {
+  const { stats } = useContext(World);
+  const count = stats && stats.notifications;
+  return (
+    <NavbarLink exact to={NOTIFICATIONS_URL}>
+      Notifications {!!count && `(${count})`}
+    </NavbarLink>
+  );
+};
+
+const SettingsNavbarLink: React.FC = () => <NavbarLink to={SETTINGS_URL}>Settings</NavbarLink>;
+
+const MainNavbarLinks: React.FC = () => (
+  <>
+    <SettingsNavbarLink />
+    <NotificationsNavbarLink />
+  </>
+);
+
+const MeNavbarItem: React.FC = () => (
+  <Navbar.Item dropdown hoverable>
+    <Navbar.Link>Me</Navbar.Link>
+    <Navbar.Dropdown direction="right">
+      <MainNavbarLinks />
+      <Navbar.Divider />
+      <Logout>
+        {({ logout }) => (
+          <Navbar.Item>
+            <Button type="button" onClick={logout}>
+              Logout
+            </Button>
+          </Navbar.Item>
+        )}
+      </Logout>
+    </Navbar.Dropdown>
+  </Navbar.Item>
+);
+
+export const Header: React.FC = () => {
+  const { session } = useContext(World);
+  const [active, setActive] = useState<boolean>(DEFAULT_ACTIVE);
   const onToggle = () => setActive(!active);
 
   return (
     <header>
       <Navbar color="light">
         <Navbar.Brand>
-          <NavLink className="navbar-item" to={ROOT_URL} activeClassName="is-active">
-            Playground
-          </NavLink>
+          <HomeNavbarLink />
           <Navbar.Burger active={active} onClick={onToggle} />
         </Navbar.Brand>
         <Navbar.Menu active={active}>
-          <Navbar.Start>
-            <NavLink exact to={ROOT_URL} className="navbar-item" activeClassName="is-active">
-              Home
-            </NavLink>
-            {session && (
-              <>
-                <NavLink to={SETTINGS_URL} className="navbar-item" activeClassName="is-active">
-                  Settings
-                </NavLink>
-                <NavLink to={NOTIFICATIONS_URL} className="navbar-item" activeClassName="is-active">
-                  Notifications {stats && !!stats.notifications && <>({stats.notifications})</>}
-                </NavLink>
-              </>
-            )}
-          </Navbar.Start>
-          <Navbar.End>
-            {!session && (
-              <>
-                <NavLink exact to={LOGIN_URL} className="navbar-item" activeClassName="is-active">
-                  Login
-                </NavLink>
-                <NavLink exact to={SIGNUP_URL} className="navbar-item" activeClassName="is-active">
-                  Signup
-                </NavLink>
-              </>
-            )}
-            {session && (
-              <>
-                <Navbar.Item dropdown hoverable>
-                  <Navbar.Link>Me</Navbar.Link>
-                  <Navbar.Dropdown direction="right">
-                    <NavLink to={SETTINGS_URL} className="navbar-item" activeClassName="is-active">
-                      Settings
-                    </NavLink>
-                    <Navbar.Divider />
-                    <Logout>
-                      {({ logout }) => (
-                        <Navbar.Item>
-                          <Button type="button" onClick={logout}>
-                            Logout
-                          </Button>
-                        </Navbar.Item>
-                      )}
-                    </Logout>
-                  </Navbar.Dropdown>
-                </Navbar.Item>
-              </>
-            )}
-          </Navbar.End>
+          <Navbar.Start>{session && <MainNavbarLinks />}</Navbar.Start>
+          <Navbar.End>{session ? <MeNavbarItem /> : <AuthenticateNavbarLinks />}</Navbar.End>
         </Navbar.Menu>
       </Navbar>
     </header>
