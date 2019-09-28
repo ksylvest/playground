b07b2634bba65db7d59e7a23fb4b5bba50a544fa
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-28 14:54:25 -0700

    Use a service for attachment variants

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,12 +1,9 @@
 class AttachmentsController < ApplicationController
-  FORMATS = %w[jpg heic webp].freeze
-  RESIZES = %w[fit fill].freeze
-  private_constant :FORMATS
-  private_constant :RESIZES
-
-  # GET /attached/:id.(jpg|png|heic|webp)?l=:l&w=:w&resize=(fit|fill)
+  # GET /attached/:id.(jpg|heic|webp)?l=:l&w=:w&resize=(fit|fill)
   def show
-    redirect_to variant.processed.service_url(disposition: params[:disposition])
+    redirect_to variant.service_url(disposition: params[:disposition])
+  rescue ArgumentError
+    head :unauthorized
   end
 
 private
@@ -15,36 +12,19 @@ private
     ActiveStorage::Attachment.find(params[:id])
   end
 
+  def service
+    Attachment::VariantService.new(
+      attachment: attachment,
+      convert: params.fetch(:format),
+      resize: params.fetch(:resize),
+      size: [
+        Integer(params.fetch(:w)),
+        Integer(params.fetch(:h)),
+      ]
+    )
+  end
+
   def variant
-    attachment.variant("resize_to_#{resize}": size, convert: format)
-  end
-
-  def size
-    [
-      w,
-      h,
-    ]
-  end
-
-  def w
-    Integer(params.fetch(:w))
-  end
-
-  def h
-    Integer(params.fetch(:h))
-  end
-
-  def format
-    format = params.fetch(:format)
-    return format if FORMATS.include?(format)
-
-    raise ArgumentError, "invalid option for format: #{format.inspect}"
-  end
-
-  def resize
-    resize = params.fetch(:resize)
-    return resize if RESIZES.include?(resize)
-
-    raise ArgumentError, "invalid option for resize: #{resize.inspect}"
+    service.variant.processed
   end
 end
