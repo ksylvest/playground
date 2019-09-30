8a17aa40d5845865bc735320bb874ab8621f0024
Kevin Sylvestre <kevin@ksylvest.com>
2019-09-30 00:51:49 -0700

    Fix missing mime type for attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,7 +2,7 @@ class AttachmentsController < ApplicationController
   # GET /attached/:id.(jpg|webp)?l=:l&w=:w&resize=(fit|fill)
   def show
     expires_in 2.years, public: !params[:fresh]
-    send_data(service.data, type: format, disposition: 'inline')
+    send_data(service.data, type: Mime::Type.lookup_by_extension(format), disposition: 'inline')
   end
 
 private
