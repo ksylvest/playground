70b0b969f3ec69e0b7067715826c65cca3b617b1
Kevin Sylvestre <kevin@ksylvest.com>
2019-10-23 09:06:21 -0700

    Install emotion for CSS

--- babel.config.js
+++ babel.config.js
@@ -44,6 +44,7 @@ module.exports = function(api) {
       ]
     ].filter(Boolean),
     plugins: [
+      'babel-plugin-emotion',
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
