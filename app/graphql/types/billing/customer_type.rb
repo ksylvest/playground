f82ace8f65806708993d149c3ed80e953f19f82e
Kevin Sylvestre <kevin@ksylvest.com>
2020-02-01 15:55:12 -0800

    GraphQL types for subscription feature

--- app/graphql/types/billing/customer_type.rb
+++ app/graphql/types/billing/customer_type.rb
@@ -6,9 +6,14 @@ module Types
       field :id, ID, null: false
       field :currency, CurrencyType, null: true
       field :sources, [SourceType], null: false
+      field :subscription, SubscriptionType, null: true
 
       def sources
         Loaders::AssociationLoader.for(:sources).load(object)
+      end
+
+      def subscription
+        Loaders::AssociationLoader.for(:subscription).load(object)
       end
     end
   end
--- app/graphql/types/billing_type.rb
+++ app/graphql/types/billing_type.rb
@@ -2,5 +2,6 @@ module Types
   class BillingType < BaseObject
     field :id, ID, null: false
     field :customer, Types::Billing::CustomerType, null: true
+    field :products, [Types::Billing::ProductType], null: false
   end
 end
