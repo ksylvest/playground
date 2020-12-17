b40b199b2bd8fc5e555dc690088ddb35b86dedbc
Kevin Sylvestre <kevin@ksylvest.com>
2020-12-17 09:31:12 -0800

    Use sentry browser / tracing

--- app/webpack/application/initializers/sentry.ts
+++ app/webpack/application/initializers/sentry.ts
@@ -1,15 +1,17 @@
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+
 declare const ENVIRONMENT: string;
 declare const RELEASE: string;
 declare const SENTRY_DSN: string | null;
 
 ((): void => {
   if (typeof window !== "undefined") {
-    const Sentry = require("@sentry/browser");
-
     if (SENTRY_DSN && ENVIRONMENT !== "test" && ENVIRONMENT !== "development") {
       Sentry.init({
         dsn: SENTRY_DSN,
         environment: ENVIRONMENT,
+        integrations: [new Integrations.BrowserTracing()],
         release: RELEASE,
       });
     }
