045999583e23ec0c791917453ea887e1ab291b61
Kevin Sylvestre <kevin@ksylvest.com>
2020-12-28 15:36:54 -0800

    Stop using sentry tracing

--- app/webpack/application/initializers/sentry.ts
+++ app/webpack/application/initializers/sentry.ts
@@ -1,10 +1,8 @@
 import * as Sentry from "@sentry/browser";
-import { Integrations } from "@sentry/tracing";
 
 declare const ENVIRONMENT: string;
 declare const RELEASE: string;
 declare const SENTRY_DSN: string | null;
-declare const SENTRY_TRACES_SAMPLE_RATE: number;
 
 ((): void => {
   if (typeof window !== "undefined") {
@@ -12,9 +10,7 @@ declare const SENTRY_TRACES_SAMPLE_RATE: number;
       Sentry.init({
         dsn: SENTRY_DSN,
         environment: ENVIRONMENT,
-        integrations: [new Integrations.BrowserTracing()],
         release: RELEASE,
-        tracesSampleRate: SENTRY_TRACES_SAMPLE_RATE,
       });
     }
   }
