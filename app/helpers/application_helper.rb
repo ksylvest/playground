64a6402e9e44cb258ffbbf6bc6c569b7d5cc520b
Kevin Sylvestre <kevin@ksylvest.com>
2021-12-02 20:29:14 -0800

    Allow for zooming

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def viewport
-    'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
+    'width=device-width, initial-scale=1.0'
   end
 
   def release
