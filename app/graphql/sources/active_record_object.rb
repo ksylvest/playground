e4727981618d16e0deaad9a28de8ceed3ebe9d2a
Kevin Sylvestre <kevin@ksylvest.com>
2021-12-19 14:02:34 -0800

    Use async_load for GQL API

--- app/graphql/sources/active_record_object.rb
+++ app/graphql/sources/active_record_object.rb
@@ -1,8 +1,11 @@
 module Sources
   class ActiveRecordObject < ActiveRecordBase
     def fetch(keys)
-      results = models(keys: keys).order(:id).index_by { |model| model[@key] }
-      keys.map { |key| results[key] }
+      models = models(keys: keys).order(:id).load_async
+      dataloader.yield
+
+      map = models.index_by { |model| model[@key] }
+      keys.map { |key| map[key] }
     end
   end
 end
