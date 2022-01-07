a0ed43f9e389afd54ad5684849bfddb5a2e2f4ac
Kevin Sylvestre <kevin@ksylvest.com>
2022-01-07 14:22:22 -0800

    Bump ruby v3.1

--- app/graphql/sources/cache.rb
+++ app/graphql/sources/cache.rb
@@ -1,8 +1,8 @@
 module Sources
   class Cache < GraphQL::Dataloader::Source
     def fetch(operations)
-      keys = operations.map { |operation| operation[:key] }
-      procs = Hash[operations.map { |operation| [operation[:key], operation[:value]] }]
+      keys = operations.pluck(:key)
+      procs = operations.to_h { |operation| [operation[:key], operation[:value]] }
       results = Rails.cache.fetch_multi(*keys) { |key| procs[key].call }
       operations.map { |operation| results[operation[:key]] }
     end
