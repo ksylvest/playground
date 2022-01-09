e8f9cc8b4e9a32cc8547b31df240de678073ef7d
Kevin Sylvestre <kevin@ksylvest.com>
2022-01-09 13:31:51 -0800

    Fix use load JS for stripe

--- app/packs/application/libraries/stripe/element.tsx
+++ app/packs/application/libraries/stripe/element.tsx
@@ -2,30 +2,12 @@ import * as React from "react";
 import { useEffect, useRef, useState } from "react";
 
 export const Element: React.FC<{
-  element: Promise<stripe.Element>;
-}> = ({ element: promise }) => {
+  element: stripe.Element | undefined;
+}> = ({ element }) => {
   const ref = useRef<HTMLDivElement>(null);
-  const [element, setElement] = useState<stripe.Element | undefined>(undefined);
 
   useEffect(() => {
-    let cancel = false;
-    (async (): Promise<void> => {
-      const result = await promise;
-      if (cancel) {
-        return;
-      }
-      setElement(result);
-    })();
-    return (): void => {
-      cancel = true;
-    };
-  }, [promise]);
-
-  useEffect(() => {
-    if (!element) {
-      return;
-    }
-    if (!ref.current) {
+    if (!element || !ref.current) {
       return;
     }
     element.mount(ref.current);
