99a1c86bdb94822678aac785b6b7e76bdaab4288
Kevin Sylvestre <kevin@ksylvest.com>
2022-04-28 10:12:50 -0700

    Use a meta tag for mapbox-access-token

--- app/packs/application/libraries/mapbox/index.ts
+++ app/packs/application/libraries/mapbox/index.ts
@@ -1,20 +1,21 @@
 import loadjs from "loadjs";
 
-declare const MAPBOX_ACCESS_TOKEN: string | null;
-
 const VERSION = "v2.0.0";
 
-if (typeof window !== "undefined" && !!MAPBOX_ACCESS_TOKEN) {
-  loadjs(
-    [
-      `https://api.mapbox.com/mapbox-gl-js/${VERSION}/mapbox-gl.js`,
-      `https://api.mapbox.com/mapbox-gl-js/${VERSION}/mapbox-gl.css`,
-    ],
-    "mapboxgl",
-    () => {
-      mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
-    }
-  );
+if (typeof window !== "undefined") {
+  const MAPBOX_ACCESS_TOKEN = document.head.querySelector('meta[name="mapbox-access-token"]').getAttribute("content");
+  if (MAPBOX_ACCESS_TOKEN) {
+    loadjs(
+      [
+        `https://api.mapbox.com/mapbox-gl-js/${VERSION}/mapbox-gl.js`,
+        `https://api.mapbox.com/mapbox-gl-js/${VERSION}/mapbox-gl.css`,
+      ],
+      "mapboxgl",
+      () => {
+        mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+      }
+    );
+  }
 }
 
 export * from "./components";
