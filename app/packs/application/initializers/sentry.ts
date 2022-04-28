831ac9475279b697c37f0b28f8d384950e8afbe1
Kevin Sylvestre <kevin@ksylvest.com>
2022-04-28 10:25:27 -0700

    Use a meta tag for sentry-dsn

--- app/packs/application/initializers/sentry.ts
+++ app/packs/application/initializers/sentry.ts
@@ -1,17 +1,15 @@
 import * as Sentry from "@sentry/browser";
 
-declare const ENVIRONMENT: string;
-declare const RELEASE: string;
-declare const SENTRY_DSN: string | null;
+if (typeof window !== "undefined") {
+  const SENTRY_DSN = document.head.querySelector('meta[name="sentry-dsn"]').getAttribute("content");
+  const ENVIRONMENT = document.head.querySelector('meta[name="environment"]').getAttribute("content");
+  const RELEASE = document.head.querySelector('meta[name="release"]').getAttribute("content");
 
-((): void => {
-  if (typeof window !== "undefined") {
-    if (SENTRY_DSN && ENVIRONMENT !== "test" && ENVIRONMENT !== "development") {
-      Sentry.init({
-        dsn: SENTRY_DSN,
-        environment: ENVIRONMENT,
-        release: RELEASE,
-      });
-    }
+  if (SENTRY_DSN && ENVIRONMENT !== "test" && ENVIRONMENT !== "development") {
+    Sentry.init({
+      dsn: SENTRY_DSN,
+      environment: ENVIRONMENT,
+      release: RELEASE,
+    });
   }
-})();
+}
