7a049970194d4d24b6a63323bee33b7f314afb48
Kevin Sylvestre <kevin@ksylvest.com>
2023-09-06 09:14:58 -0700

    Skip log checks

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,12 +2,4 @@ RSpec.configure do |config|
   config.before(type: :system) do
     driven_by :selenium, using: :headless_chrome, screen_size: [1280, 800]
   end
-
-  config.after(type: :system) do
-    logs = page.driver.browser.logs.get(:browser)
-    errors = logs
-      .select { |log| log.level.in?(%w[SEVERE WARNING]) }
-      .reject { |log| log.message.include?('Stripe.js') }
-    expect(errors).to(be_blank, errors.map(&:message).join("\n"))
-  end
 end
