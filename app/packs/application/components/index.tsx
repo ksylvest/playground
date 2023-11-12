a31464dbe631856c8125c2badecab1267007bc70
Kevin Sylvestre <kevin@ksylvest.com>
2023-11-12 14:21:50 -0800

    Fix react warning with render usage

--- app/packs/application/components/index.tsx
+++ app/packs/application/components/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { App } from "./app";
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  ReactDOM.render(<App />, document.getElementById("app"));
+addEventListener("DOMContentLoaded", (event) => {
+  const container = document.getElementById("app");
+  const root = createRoot(container!);
+  root.render(<App />);
 });
