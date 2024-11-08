7c7ca5e5c632162b4e8dcbd45c4121cba01b6168
Kevin Sylvestre <kevin@ksylvest.com>
2024-11-07 19:57:25 -0800

    Bump rails from v7.2 to v8.0

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module Playground
   class Application < Rails::Application
-    config.load_defaults 7.1
+    config.load_defaults 8.0
 
     config.autoload_lib(ignore: %w[assets tasks])
 
