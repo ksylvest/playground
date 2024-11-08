f1f7dd59450a9bbb5368178a480d1f82d7d98a44
Kevin Sylvestre <kevin@ksylvest.com>
2024-11-08 08:48:48 -0800

    Re-generate rails defaults

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module Playground
       g.helper false
       g.stylesheets false
       g.javascripts false
-      g.template_engine :slim
       g.test_framework :rspec, fixture: true, views: false
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
