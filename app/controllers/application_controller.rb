d3d40e9b1eb194aa150a710419ed51f660b52d6a
Kevin Sylvestre <kevin@ksylvest.com>
2024-11-08 09:14:17 -0800

    Use remote_ip instead of IP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action { Current.ip = request.ip }
+  before_action { Current.ip = request.remote_ip }
 end
